7891b60380ec93fdfdfc0ce14d58a1f7d68cd6ce
easilar <easilar@gmail.com>
2020-10-06 14:26:35 +0200

    script added for slurm submition

---
+++ prod_mini.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export nEvents=$2
+export Higgs_mass=$1
+
+echo "Higgs mass: $Higgs_mass"
+echo "nEvents: $nEvents"
+
+export SCRAM_ARCH=slc7_amd64_gcc820;
+echo $VO_CMS_SW_DIR;
+source $VO_CMS_SW_DIR/cmsset_default.sh;
+cd /home/cms/asilar/prod_nano/CMSSW_10_2_22/src;
+pwd
+cmsenv
+
+cmsDriver.py step3 --python_filename Step_MiniAOD_cfg_"$Higgs_mass"_.py --conditions auto:phase1_2017_realistic --fast  -n "$nEvents" --era Run2_2017_FastSim --eventcontent MINIAODSIM --runUnscheduled  --filein file:/gridgroup/cms/htt/shared_files/Data/AODSIM/Htt_"$Higgs_mass"_AODSIM.root --fileout file:/gridgroup/cms/htt/shared_files/Data/MiniAODSIM/Htt_"$Higgs_mass"_MiniAODSIM.root -s PAT --datatier MINIAODSIM --mc
+
+
---
+++ submit_single.sh
@@ -0,0 +1,109 @@
+#!/bin/bash
+#submit_single.sh
+#
+#SBATCH --job-name=NP_"$mass"
+#SBATCH --output=/home/cms/asilar/slurmJobs/submitted_"$mass"
+#
+# mail-type=BEGIN, END, FAIL, REQUEUE, ALL, STAGE_OUT, TIME_LIMIT_90
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=e.asilar@ipnl.in2p3.fr
+
+echo
+echo
+echo -n "Hostname:              " ; hostname -f
+echo -n "Uptime:                " ; uptime
+echo
+echo -n "Je suis:               "; id -a
+echo
+
+oldLANG=$LANG
+LANG=fr_FR.iso88591
+date
+echo
+temps=$(grid-proxy-info -timeleft 2>/dev/null)
+if [ $? == 0 ]; then
+  echo -n "Temps restant : $temps secondes, soit dans "; date -u -d @"$temps" +'%-Hh %-Mm %-Ss'
+  echo -n "Le proxy expirera le "; date -d "now + $temps seconds" +"%A %d %B a %T"
+else
+  echo "Pas de proxy valide"
+fi
+
+LANG=$oldLANG
+
+# Check if Globus certificate is expiring soon
+CERT="$HOME/.globus/usercert.pem"
+which openssl > /dev/null 2>&1
+if [ $? == 0 ]; then
+  if [ -r "$CERT" ]; then
+    openssl x509 -in "$CERT" -noout -checkend 0 > /dev/null 2>&1
+    if [ $? == 1 ]; then
+      MSG="Your certificate has expired"
+    else
+      #604800=60*60*24*7
+      openssl x509 -in "$CERT" -noout -checkend 604800 > /dev/null 2>&1
+      if [ $? == 1 ]; then
+	MSG="Your certificate is going to expire before one week"
+      fi
+    fi
+  else
+    MSG="Can't find certificate $CERT"
+  fi
+fi
+
+
+sh prod_aod.sh  $mass $nevents
+sh prod_mini.sh $mass $nevents
+sh prod_nano.sh $mass $nevents
+
+lscpu
+
+echo
+echo "espace dans TMPDIR (TMPDIR=$TMPDIR) :"
+df -h $TMPDIR
+
+echo
+echo "espace dans HOME (HOME=$HOME)"
+df -h $HOME
+
+echo
+echo "espace dans le repertoire local (.=$PWD)"
+df -h $PWD
+
+echo
+echo "versions des variables d'environnement MPI : "
+env | grep MPI_ | grep VERSION
+
+echo
+echo "versions des librairies MPI : "
+rpm -qa|grep mpi
+
+echo
+echo "environnement :"
+env
+
+
+echo "SLURM Input environment variables : "
+echo SLURM_JOB_NAME=$SLURM_JOB_NAME
+echo SLURM_JOB_NUM_NODES=$SLURM_JOB_NUM_NODES
+echo SLURM_MEM_PER_CPU=$SLURM_MEM_PER_CPU
+echo SLURM_MEM_PER_NODE=$SLURM_MEM_PER_NODE
+echo SLURM_NTASKS=$SLURM_NTASKS
+echo SLURM_NTASKS_PER_CORE=$SLURM_NTASKS_PER_CORE
+echo SLURM_NTASKS_PER_NODE=$SLURM_NTASKS_PER_NODE
+echo SLURM_NTASKS_PER_SOCKET=$SLURM_NTASKS_PER_SOCKET
+echo SLURM_THREADS=$SLURM_THREADS
+echo SLURM_WORKING_DIR=$SLURM_WORKING_DIR
+echo SLURM_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
+echo SLURM_ARRAY_TASK_COUNT=$SLURM_ARRAY_TASK_COUNT
+echo SLURM_ARRAY_TASK_ID=$SLURM_ARRAY_TASK_ID
+echo SLURM_ARRAY_TASK_STEP=$SLURM_ARRAY_TASK_STEP
+echo SLURM_ARRAY_JOB_ID=$SLURM_ARRAY_JOB_ID
+echo
+echo "SLURM Output environment variables : "
+echo SLURM_CPUS_ON_NODE=$SLURM_CPUS_ON_NODE
+echo SLURM_JOB_CPUS_PER_NODE=$SLURM_JOB_CPUS_PER_NODE
+echo SLURM_CPUS_PER_TASK=$SLURM_CPUS_PER_TASK
+echo SLURM_JOB_ID=$SLURM_JOB_ID
+echo SLURM_JOB_NODELIST=$SLURM_JOB_NODELIST
+echo SLURM_NTASKS=$SLURM_NTASKS
+
